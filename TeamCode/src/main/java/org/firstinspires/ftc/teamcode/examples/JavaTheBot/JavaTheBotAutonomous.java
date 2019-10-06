e46adf69c0794a242b5c73f12189862bb67e6495
ni <>
2019-10-06 11:30:54 -0700

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/JavaTheBot/JavaTheBotAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/JavaTheBot/JavaTheBotAutonomous.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import java.lang.Math.*;
 @Autonomous(name="JavaTheBotAutonomous")
 
 public class JavaTheBotAutonomous extends LinearOpMode {
@@ -12,27 +12,75 @@ public class JavaTheBotAutonomous extends LinearOpMode {
     DcMotor left;
     DcMotor right;
 
+    final static double SPEED = 37.25;
+    final static boolean LEFT = true;
+    final static boolean RIGHT = false;
     @Override
     public void runOpMode() {
 
-        left = hardwareMap.dcMotor.get("left");
-        right = hardwareMap.dcMotor.get("right");
+        left = hardwareMap.dcMotor.get("LeftMotor");
+        right = hardwareMap.dcMotor.get("RightMotor");
 
         waitForStart();
+//
+//        left.setPower(1);
+//        right.setPower(1);
+//        sleep(1333);
 
-        left.setPower(1);
-        right.setPower(-1);
-        sleep(2000);
+//        left.setPower(-1);
+//        right.setPower(1);
+//        sleep(2000);
+//
+//        left.setPower(1);
+//        right.setPower(1);
+//        sleep(2000);
+//
+//
+        moveForward(12);
 
-        left.setPower(-1);
-        right.setPower(1);
-        sleep(2000);
+        sleep(3000);
 
-        left.setPower(1);
-        right.setPower(1);
-        sleep(2000);
+        turn(90, LEFT);
+
+        sleep(3000);
+
+        moveForward(24);
+
+        sleep(3000);
+
+        turn(90, RIGHT);
 
         stop();
 
+    }
+
+    public void moveBackward(double distance) {
+
+    }
+
+    public void moveForward(double distance) {
+        double sleepTime = distance/SPEED * 1000;
+        left.setPower(0.5);
+        right.setPower(-0.5);
+        sleep((long) sleepTime);
+        left.setPower(0);
+        right.setPower(0);
+    }
+
+    public void turn(double amount, boolean direction) {
+
+        double degrees = amount/45*333;
+
+        if (direction = LEFT){
+            left.setPower(-1);
+            right.setPower(-1);
+            sleep((long) degrees);
+        }else {
+            left.setPower(1);
+            right.setPower(1);
+            sleep((long) degrees);
+        }
+        left.setPower(0);
+        right.setPower(0);
     }
 }
