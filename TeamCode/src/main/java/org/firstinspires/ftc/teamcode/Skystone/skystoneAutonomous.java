1966438353b6e3073c4ee14d044b632d0b9d0deb
Andrew Z <andrewluzhao@gmail.com>
2019-10-12 07:34:25 -0700

    test push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/skystoneAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/skystoneAutonomous.java
@@ -1,28 +1,35 @@
-package org.firstinspires.ftc.teamcode.Skystone;
+//package org.firstinspires.ftc.teamcode.Skystone;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//
+//
+//public class SkystoneAutonomous extends LinearOpMode {
+//
+//
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "Skystone Demo")
-public class skystoneAutonomous extends LinearOpMode {
-
-    // Create motors in program
-    private DcMotor left;
-    private DcMotor right;
-
-    @Override
-    public void runOpMode() {
-
-        basicChassis myChassis = new basicChassis();
-
-        waitForStart();
-
-        myChassis.moveForward(12);
-
-        myChassis.moveForward(5);
-        stop();
-
-    }
-}
+//    public static void main() {
+//
+//        SimpleChassis myChassis = new SimpleChassis();
+//
+//        myChassis.moveForward(12);
+//
+//        myChassis.sleep(3000);
+//
+//        myChassis.turn(90, myChassis.LEFT);
+//
+//        myChassis.sleep(3000);
+//
+//        myChassis.moveForward(24);
+//
+//        sleep(3000);
+//
+//        myChassis.turn(90, myChassis.RIGHT);
+//
+//        stop();
+//
+//    }
+//}
